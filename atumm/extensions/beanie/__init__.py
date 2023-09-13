0c93bb12c08ff58aebf670d3d79640b4021db200
omars44 <omar@atumm.org>
2023-09-13 13:23:02 +0300

    beanie: delegate model registry to the caller

--- atumm/extensions/beanie/__init__.py
+++ atumm/extensions/beanie/__init__.py
@@ -1,13 +1,14 @@
 from atumm.services.user.dataproviders.beanie.models import User
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from beanie.odm.documents import Document
 
 
 # todo: refactor - pass models, and make a model collector
 async def init_my_beanie(
-    client: AsyncIOMotorClient, db_name: str
+    client: AsyncIOMotorClient, db_name: str, models: List[Document]
 ) -> AsyncIOMotorDatabase:
     db = client[db_name]
 
-    await init_beanie(db, document_models=[User])
+    await init_beanie(db, document_models=models)
     return db
