2332c42f25468e2264a5a60f57e701c257f84a63
omars44 <omar@atumm.org>
2023-09-16 10:31:10 +0300

    @deprecated(msg): decorator to warn of deprecated function usage

---
+++ atumm/extensions/util/__init__.py
@@ -0,0 +1,12 @@
+import warnings
+from typing import List, Callable
+
+def deprecated(msg: str) -> Callable:
+    """This is a decorator which can be used to mark functions as deprecated.
+    It will result in a warning being emitted when the function is used."""
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs) -> Callable:
+            warnings.warn(f"{msg}", category=DeprecationWarning, stacklevel=2)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
