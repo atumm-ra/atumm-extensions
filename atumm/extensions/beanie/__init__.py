e75926fedd69271b7fb839ee4354c9f673e68891
omars44 <omar@atumm.org>
2023-09-16 10:30:43 +0300

    prefer beanie lifespan ctxmanager instead of a function

--- atumm/extensions/beanie/__init__.py
+++ atumm/extensions/beanie/__init__.py
@@ -1,14 +1,30 @@
+from atumm.core.types import AsyncContextManager
 from atumm.services.user.dataproviders.beanie.models import User
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from beanie.odm.documents import Document
+from builtins import BaseException
+from typing import Type, Optional
+from atumm.extensions.beanie import init_my_beanie
 
-
-# todo: refactor - pass models, and make a model collector
 async def init_my_beanie(
-    client: AsyncIOMotorClient, db_name: str, models: List[Document]
+    client: AsyncIOMotorClient, db_name: str, documents: List[Document]
 ) -> AsyncIOMotorDatabase:
     db = client[db_name]
 
-    await init_beanie(db, document_models=models)
+    await init_beanie(db, document_models=documents)
     return db
+
+
+class BeanieLifespan(AsyncContextManager):
+
+    def __init__(self, client: AsyncIOMotorClient, db_name: str, documents: List[Document]):
+        self.beanie_client = client
+        self.db_name = db_name
+        self.documents = documents
+    
+    async def __aenter__(self) -> AsyncIOMotorDatabase:
+        return await init_my_beanie(client=self.beanie_client, db_name=self.db_name, documents=self.documents)
+
+    async def __aexit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], traceback: Any) -> None:
+        await self.beanie_client.close()
