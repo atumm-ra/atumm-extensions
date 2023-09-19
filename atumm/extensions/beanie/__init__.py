d3cc8f42742587f59bd92a4fd18e0e113565b73c
omars44 <omar@atumm.org>
2023-09-19 10:03:22 +0300

    code formatting

--- atumm/extensions/beanie/__init__.py
+++ atumm/extensions/beanie/__init__.py
@@ -1,11 +1,13 @@
+from builtins import BaseException
+from typing import Optional, Type
+
 from atumm.core.types import AsyncContextManager
+from atumm.extensions.beanie import init_my_beanie
 from atumm.services.user.dataproviders.beanie.models import User
 from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from beanie.odm.documents import Document
-from builtins import BaseException
-from typing import Type, Optional
-from atumm.extensions.beanie import init_my_beanie
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
 
 async def init_my_beanie(
     client: AsyncIOMotorClient, db_name: str, documents: List[Document]
@@ -17,14 +19,22 @@ async def init_my_beanie(
 
 
 class BeanieLifespan(AsyncContextManager):
-
-    def __init__(self, client: AsyncIOMotorClient, db_name: str, documents: List[Document]):
+    def __init__(
+        self, client: AsyncIOMotorClient, db_name: str, documents: List[Document]
+    ):
         self.beanie_client = client
         self.db_name = db_name
         self.documents = documents
-    
-    async def __aenter__(self) -> AsyncIOMotorDatabase:
-        return await init_my_beanie(client=self.beanie_client, db_name=self.db_name, documents=self.documents)
 
-    async def __aexit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], traceback: Any) -> None:
+    async def __aenter__(self) -> AsyncIOMotorDatabase:
+        return await init_my_beanie(
+            client=self.beanie_client, db_name=self.db_name, documents=self.documents
+        )
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Any,
+    ) -> None:
         await self.beanie_client.close()
--- atumm/extensions/util/__init__.py
+++ atumm/extensions/util/__init__.py
@@ -1,12 +1,16 @@
 import warnings
-from typing import List, Callable
+from typing import Callable, List
+
 
 def deprecated(msg: str) -> Callable:
     """This is a decorator which can be used to mark functions as deprecated.
     It will result in a warning being emitted when the function is used."""
+
     def decorator(func: Callable) -> Callable:
         def wrapper(*args, **kwargs) -> Callable:
             warnings.warn(f"{msg}", category=DeprecationWarning, stacklevel=2)
             return func(*args, **kwargs)
+
         return wrapper
+
     return decorator
