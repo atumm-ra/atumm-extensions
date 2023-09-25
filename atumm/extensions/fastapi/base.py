1540b300181407afbeb24e56fd6e0bfe13b9bdc7
omars44 <omar@atumm.org>
2023-09-25 13:12:30 +0300

    swagger assets: change CDN

--- atumm/extensions/fastapi/base.py
+++ atumm/extensions/fastapi/base.py
@@ -13,6 +13,10 @@ class BaseWebApp:
             version=self.config.API_VERSION,
             docs_url=None if self.config.STAGE == "prod" else "/docs",
             redoc_url=None if self.config.STAGE == "prod" else "/redoc",
+            swagger_ui_parameters={
+                "swagger_js_url": "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js",
+                "swagger_css_url": "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.css",
+            },
         )
 
 
